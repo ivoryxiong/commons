32d1c17d53c19ada6255b87796f09705199c999e
Xiong Hua <ivoryxiong@gmail.com>
2012-07-20 16:30:29 +0800

    ADD: add string search interface

---
+++ src/main/java/org/ivory/commons/algo/string/Searcher.java
@@ -0,0 +1,10 @@
+package org.ivory.commoms.algo.string;
+
+/**
+Simple interface for single string match
+
+@author ivoryxiong
+*/
+public interface Searcher{
+    public int indexOf(char[] text, char[] pattern);
+}
